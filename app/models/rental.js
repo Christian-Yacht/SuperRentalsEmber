8e029114cff650fb9cb7536cca5d31e131d589a4
Christian van den Broeck <c.v.d.broeck@wis.nl>
2019-08-22 09:09:39 +0200

    Start to Creation of Complex Component

--- app/models/rental.js
+++ app/models/rental.js
@@ -3,4 +3,10 @@ const { Model } = DS;
 
 export default Model.extend({
     title: DS.attr(),
+    owner: DS.attr(),
+    city: DS.attr(),
+    category: DS.attr(),
+    bedrooms: DS.attr(),
+    image: DS.attr(),
+    description: DS.attr()
 });
